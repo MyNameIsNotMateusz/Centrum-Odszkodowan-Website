eba7a2ac2824f2c506dc5b51e8bcc9c06e7ddd84
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-12-27 16:14:57 +0100

    I finished mail

--- script.js
+++ script.js
@@ -11,36 +11,41 @@ const result = document.querySelector(".result");
 sendBtn.addEventListener("click", sendEmail);
 
 function sendEmail() {
-    //Get the form data
+    const form = document.querySelector(".contact-left");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+    });
+
+    // Get the form data
     const to = "ostrycharzodszkodowania@gmail.com";
     const message = document.getElementById("message").value;
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
-    const mail = document.getElementById("mail").value
+    const mail = document.getElementById("mail").value;
     const fullName = name + " " + surname;
     const subject = "Nowa wiadomość od " + fullName + " - Kontakt: " + mail;
 
-    
-
-    //check if the fields are completed
-
+    // Check if the fields are completed
     if (name && surname && mail && message !== "") {
-
         // Send the email using EmailJS
         emailjs.send("service_ikrwart", "template_50f4sgn", {
             to_email: to,
             subject: subject,
             message: message
-        })
-            .then(function () {
-                result.innerHTML = "Email sent successfully!";
-                result.style.opacity = 1;
-            }, function (error) {
-                result.innerHTML = "Email sending failed!";
-                result.style.opacity = 1;
-            });
+        }).then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
 
-
-        
+            // Clear the form fields after successful sending
+            document.getElementById("name").value = "";
+            document.getElementById("surname").value = "";
+            document.getElementById("mail").value = "";
+            document.getElementById("message").value = "";
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
     }
 }
+
+
